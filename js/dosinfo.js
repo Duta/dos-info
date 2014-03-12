9f3e3b74a6530f3e3ae37f8cc61fff824433ca79
TheHarryCross <harry@harrycross.com>
2014-03-12 23:19:38 +0000

    fading-in swag
    yeah the codes awful

--- js/dosinfo.js
+++ js/dosinfo.js
@@ -1,3 +1,18 @@
 window.onload = function() {
 	// TODO
+	fadeInMain();
 };
+
+
+var op1 = 0;
+function fadeInMain(){
+	op1 ++;
+	document.getElementById("wrapper").style.filter = "alpha(opacity="+op1+")";
+	document.getElementById("wrapper").style.opacity = ( op1 / 100 );
+    document.getElementById("wrapper").style.MozOpacity = ( op1 / 100 );
+    document.getElementById("wrapper").style.KhtmlOpacity = ( op1 / 100 );
+	if(op1 < 100){
+		window.setTimeout('fadeInMain()',2);
+	}
+}
+
