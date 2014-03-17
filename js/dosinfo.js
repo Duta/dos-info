db125c4317f1d14e282b1c1e5a56f663c79ad5f2
Bertie Wheen <wheen.b@gmail.com>
2014-03-17 23:32:58 +0000

    Loading pages with AJAX

--- js/dosinfo.js
+++ js/dosinfo.js
@@ -2,6 +2,31 @@ $(function() {
     $('#wrapper').hide();
     $('#wrapper').fadeIn(1000);
     $('#leftsidebar .sidebarbutton').click(function() {
-        alert('Loading ' + $(this).html());
+        var file = this.id + '.xml';
+        $('#wrapper').fadeOut(500, function() {
+            $('#maincontent').children().remove();
+            $.ajax({
+                url: 'pages/' + file,
+                type: 'GET',
+                dataType: 'xml',
+                success: function(xml) {
+                    $(xml).find('section').each(function() {
+                        var elem = $('<div class="section"></div>');
+                        $(this).find('paragraph').each(function() {
+                            $('<p></p>').html($(this).text()).appendTo(elem);
+                        });
+                        elem.appendTo('#maincontent');
+                    });
+                },
+                error: function() {
+                    $('<div class="section"></div>')
+                        .html('<p>Error loading ' + file + '</p>')
+                        .appendTo('#maincontent');
+                },
+                complete: function() {
+                    $('#wrapper').fadeIn(500);
+                }
+            });
+        });
     });
 });
