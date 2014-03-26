fd962395de5c050e54ab848f0d32b576a90738d4
Duta <wheen.b@gmail.com>
2014-03-26 12:47:56 +0000

    Loads intro page by default

--- js/dosinfo.js
+++ js/dosinfo.js
@@ -1,31 +1,41 @@
+function loadXML(file, oncomplete) {
+    $.ajax({
+        url: 'pages/' + file,
+        type: 'GET',
+        dataType: 'xml',
+        success: function(xml) {
+            $(xml).find('section').each(function() {
+                var elem = $('<div class="section"></div>');
+                $(this).find('paragraph').each(function() {
+                    $('<p></p>').html($(this).text()).appendTo(elem);
+                });
+                elem.appendTo('#maincontent');
+            });
+        },
+        error: function() {
+            $('<div class="section"></div>')
+                .html('<p>Error loading ' + file + '</p>')
+                .appendTo('#maincontent');
+        },
+        complete: oncomplete
+    });
+}
+
+function loadPage(page, oncomplete) {
+    loadXML(page + '.xml', oncomplete);
+}
+
 $(function() {
     $('#wrapper').hide();
-    $('#wrapper').fadeIn(1000);
+    loadPage('intro', function() {
+        $('#wrapper').fadeIn(1000);
+    });
     $('#leftsidebar .sidebarbutton').click(function() {
-        var file = this.id + '.xml';
+        var file = this.id;
         $('#wrapper').fadeOut(500, function() {
             $('#maincontent').children().remove();
-            $.ajax({
-                url: 'pages/' + file,
-                type: 'GET',
-                dataType: 'xml',
-                success: function(xml) {
-                    $(xml).find('section').each(function() {
-                        var elem = $('<div class="section"></div>');
-                        $(this).find('paragraph').each(function() {
-                            $('<p></p>').html($(this).text()).appendTo(elem);
-                        });
-                        elem.appendTo('#maincontent');
-                    });
-                },
-                error: function() {
-                    $('<div class="section"></div>')
-                        .html('<p>Error loading ' + file + '</p>')
-                        .appendTo('#maincontent');
-                },
-                complete: function() {
-                    $('#wrapper').fadeIn(500);
-                }
+            loadPage(file, function() {
+                $('#wrapper').fadeIn(500);
             });
         });
     });
